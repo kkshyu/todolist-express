b99b4d6b6e4bff6bfa217c00e37de5a61e97f315
徐愷 <kkshyu.tw@gmail.com>
2020-08-22 12:24:28 +0800

    [FEATURE] add fields and pagination

--- routes/todos.js
+++ routes/todos.js
@@ -1,18 +1,35 @@
 var express = require("express");
 var router = express.Router();
 
-const todos = [
-  { id: 0, content: "xxx", done: false },
-  { id: 1, content: "123", done: true },
-  { id: 2, content: "4235", done: true },
-  { id: 3, content: "346", done: false },
-  { id: 4, content: "765", done: false },
+let todos = [
+  { id: 0, content: "sample1", done: false },
+  { id: 1, content: "sample2", done: true },
+  { id: 2, content: "sample3", done: false },
 ];
 
-// get all todos
-router.get("/", function (req, res, next) {
+// get all todo items
+router.get("/", (req, res, next) => {
   const query = req.query;
-  res.send(todos);
+  const start = parseInt(query.offset) || 0;
+  const end = query.limit ? start + parseInt(query.limit) : todos.length;
+  const filteredTodos = todos.slice(start, end).map((todo) => {
+    if (query.fields) {
+      const fieldTodo = {};
+      query.fields.split(",").forEach((field) => {
+        fieldTodo[field] = todo[field];
+      });
+      return fieldTodo;
+    } else {
+      return todo;
+    }
+  });
+  res.send(filteredTodos);
+});
+
+// delete all todo items
+router.delete("/", (req, res) => {
+  todos = [];
+  res.send();
 });
 
 // create a new todo
@@ -25,6 +42,30 @@ router.post("/", function (req, res, next) {
   };
   todos.push(todo);
   res.send(todos);
+});
+
+// get specific todo item
+router.get("/:todoId", (req, res, next) => {
+  const todoId = req.params.todoId;
+  res.send(todos[todoId]);
+});
+
+// update specific todo item
+router.put("/:todoId", (req, res, next) => {
+  const body = req.body;
+  const todoId = req.params.todoId;
+  todos[todoId] = {
+    ...todos[todoId],
+    ...body,
+  };
+  res.send(todos[todoId]);
+});
+
+// delete specific todo item
+router.delete("/:todoId", (req, res) => {
+  const todoId = req.params.todoId;
+  todos = [...todos.slice(0, todoId), ...todos.slice(todoId + 1)];
+  res.send();
 });
 
 module.exports = router;
