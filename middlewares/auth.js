8fba5840c22d49b81ab37f978c030bb5cd3b2e7c
徐愷 <kkshyu.tw@gmail.com>
2020-08-22 21:20:34 +0800

    [FEATURE] add http basic auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,21 @@
 const jwt = require("express-jwt");
 const basicAuth = require("express-basic-auth");
 
-const authMiddleware = (req, res, next) => {
-  if (!req.session.email) {
-    return res.send({
-      code: "E_UNAUTHORIZED",
-      message: "you have to log in first",
-      result: null,
-    });
-  }
-  next();
-};
+// const authMiddleware = (req, res, next) => {
+//   if (!req.session.email) {
+//     return res.send({
+//       code: "E_UNAUTHORIZED",
+//       message: "you have to log in first",
+//       result: null,
+//     });
+//   }
+//   next();
+// };
+
+// basic auth
+const authMiddleware = basicAuth({
+  challenge: true,
+  users: { admin: "admin", guest: "guest" },
+});
+
 module.exports = authMiddleware;
